fcac48caaab40dd11990f8d4e2a67e0836cb9877
NIKU <niku@Mac-mini.local>
2020-04-18 23:53:43 +0900

    URLとフォルダ名とページ数のコマンドライン入力に対応

--- dl.py
+++ dl.py
@@ -5,25 +5,30 @@ import urllib.request
 import sys
 import os
 
-def download():
-    path = "./pic/"
-    # os.mkdir(path)
-
-    url = "https://static8.hentai-image.com/upload/20191021/594/608222/"
+def download(folderpath, url, pages):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
 
-    for i in range(100):
+    for i in range(pages):
         try:
             # url_all = url + str(i+1).zfill(3) + ".jpg"
-            url_all = url + str(i+1) + ".jpg"
-            print (url_all)
-            urllib.request.urlretrieve(url_all, path + str(i) + ".jpg")
-            # urllib.request.urlretrieve(url_all, path + str(i).zfill(3) + ".jpg")
+            url_pix = url + str(i+1) + ".jpg"
+            print(folderpath)
+            urllib.request.urlretrieve(url_pix, folderpath + str(i) + ".jpg")
         except urllib.error.URLError as e:
-        	print (e)
         	break
 
 if __name__ == "__main__":
-    download()
+    print("please enter folder name")
+    foldername=input("--->")
+    folderpath = "./" + foldername + "/"
+    os.makedirs(folderpath, exist_ok=True)
+    
+    print("pleae enter URL")
+    # url=input("--->")
+    url = "https://static8.hentai-image.com/upload/20191021/594/608222/"
+    
+    print("please enter number of pages")
+    pages=int(input("--->"))
+    download(folderpath, url, pages)
